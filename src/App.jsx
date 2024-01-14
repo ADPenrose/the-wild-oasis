82914fb723898a14c24fef9379fe42894010fc24
Arturo Avila <mysticmetromx@gmail.com>
2024-01-14 12:35:22 -0600

    Added the basic app layout using CSS Grid

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import PageNotFound from './pages/PageNotFound';
 
 // Importing the global styles component.
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 
 // Since we are not going to be using the data loading
 // functionality of React Router, we can just use the
@@ -23,14 +24,17 @@ function App() {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
-					{/* Creating a redirect to the dashboard page */}
-					<Route index element={<Navigate to="dashboard" replace />} />
-					<Route path="dashboard" element={<Dashboard />} />
-					<Route path="bookings" element={<Bookings />} />
-					<Route path="cabins" element={<Cabins />} />
-					<Route path="users" element={<Users />} />
-					<Route path="settings" element={<Settings />} />
-					<Route path="account" element={<Account />} />
+					{/* This route is here to provide a layout to its child routes */}
+					<Route element={<AppLayout />}>
+						{/* Creating a redirect to the dashboard page */}
+						<Route index element={<Navigate to="dashboard" replace />} />
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="bookings" element={<Bookings />} />
+						<Route path="cabins" element={<Cabins />} />
+						<Route path="users" element={<Users />} />
+						<Route path="settings" element={<Settings />} />
+						<Route path="account" element={<Account />} />
+					</Route>
 					<Route path="login" element={<Login />} />
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
